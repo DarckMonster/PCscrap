b2beceec4360e570ba54111c45269e8fb3d15219
spmanolo <manolosan2001@gmail.com>
2023-11-02 17:53:17 +0100

    Añadido nombre descriptivo closes #16 Para que al usar se sepa que es un valor en Euros.

--- src/componente.ts
+++ src/componente.ts
@@ -3,6 +3,6 @@ import { TipoComponente } from "./tipo_componente";
 export class Componente {
   nombre: string;
   tipo: TipoComponente;
-  precioMax: number;
-  precioMin: number;
+  precioMaxEuros: number;
+  precioMinEuros: number;
 }
