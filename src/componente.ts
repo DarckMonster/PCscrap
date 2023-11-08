062e1e266ab8157f4dc26330865ace0812a8ed51
spmanolo <manolosan2001@gmail.com>
2023-11-08 15:51:11 +0100

    Elimina ficheros por código inválido #14 Elimina entidad y lenguaje en iv.yaml y no se borra para permitir el trabajo del siguiente objetivo.

--- src/componente.ts
+++
@@ -1,8 +0,0 @@
-import { TipoComponente } from "./tipo_componente";
-
-export class Componente {
-  nombre: string;
-  tipo: TipoComponente;
-  precioMaxEuros: number;
-  precioMinEuros: number;
-}
--- src/scraping.ts
+++
@@ -1,7 +0,0 @@
-import { Componente } from "./componente";
-
-class scraping {
-  componente: Componente;
-  tiendas: Map<string, string>;
-  listadoPrecios: Map<string, number>;
-}
