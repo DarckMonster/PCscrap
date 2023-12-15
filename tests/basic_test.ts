f4efdb610d9558c5b4c57b1b0a8d1a730d8fd7e9
DarckMonster <juanfcv2001@hotmail.com>
2023-12-15 13:46:00 +0100

    #32 Creación de archivo de tests basic_test.ts

---
+++ tests/basic_test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "bun:test";
+import { Scraping } from "../src/scraping"
+
+
+const scraping = new Scraping();
+
+test("PcComponentes", () => {
+    expect().toBe();
+});
