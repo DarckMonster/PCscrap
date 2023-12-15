fc4f5993e3250b8e6d1fb16ec23500fdf239e73b
DarckMonster <juanfcv2001@hotmail.com>
2023-12-15 12:47:51 +0100

    Reseteo de commits

--- src/componente.ts
+++ src/componente.ts
@@ -1,37 +1,7 @@
-import { Mode } from "fs";
-import { Modelos, TipoComponente } from "./tipo_componente";
+import { TipoComponente } from "./tipo_componente";
 
 export class Componente {
   private nombre: string;
   private tipo: TipoComponente;
-  private modelo: Modelos;
   private precioEur: number;
-
-  getNombre() : string {
-    return this.nombre;
-  }
-
-  getTipo() : TipoComponente {
-    return this.tipo;
-  }
-
-  getModelo() : Modelos {
-    return this.modelo;
-  }
-
-  getPrecio() : number {
-    return this.precioEur;
-  }
-
-  constructor(
-    nombre: string,
-    tipo: TipoComponente,
-    modelo: Modelos,
-    precioEur: number
-  ) {
-    this.nombre = nombre;
-    this.tipo = tipo;
-    this.modelo = modelo;
-    this.precioEur = precioEur;  
-  }
 }
--- src/tienda.ts
+++ src/tienda.ts
@@ -1,12 +1,4 @@
 export class Tienda {
   URL: string;
   PATH: string[];
-
-  constructor (
-    URL: string,
-    PATH: string[]
-  ) {
-    this.URL=URL;
-    this.PATH=PATH;
-  }
 }
